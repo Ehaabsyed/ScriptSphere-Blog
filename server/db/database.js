684590ca3dc8121a162880efe9e510cf94e98ff3
Ehaabsyed <syedehaab12@gmail.com>
2025-07-15 15:19:06 +0530

    changes

--- server/db/database.js
+++ server/db/database.js
@@ -4,7 +4,7 @@ import { configDotenv } from "dotenv";
 configDotenv()
 const ConnectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://syedehaab12:Syed@100>@syed-0047.ndh3l2g.mongodb.net/ScriptSphere?retryWrites=true&w=majority&appName=SYED-0047');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB successfully connected!");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
